29991dff0273e1ad63816aaead6d37171a0db8f1
Jordan Stornelli <jordan_stornelli@brown.edu>
2024-02-24 17:37:40 -0500

    pair prog jle37: adding mocked datasets and attempting a map from mock csv file paths

---
+++ mock/data/dataMap.ts
@@ -0,0 +1,5 @@
+export function DataMap() {
+    let dataMap = new Map<string, Array<string | number | boolean>>([
+        [""]
+    ])
+}
---
+++ mock/data/mockedJson.ts
@@ -0,0 +1,24 @@
+export const exampleHouses = [
+    ["Type", "id", "Price", "Acres", "Color"],
+    ["Mansion", 1, 10000, 100, "white"],
+    ["Hut", 2, 100, 2, "brown"],
+    ["Chateau", 3, 5000, 50, "beige"],
+    ["Shack", 4, 5, 1, "rust"]
+  ];
+
+export const exampleNeighborhoods = [
+    ["Name", "IsGated", "School District", "Number of Houses"],
+    ["Brooksview", true, "Brockboard", 15],
+    ["Corol Row", false, "Amnesty", 25],
+    ["Marcher's Ridge", false, "McMillan", 30],
+    ["Flower Way", false, "Amnesty", 21],
+    ["Peach Drive", false, "McMillan", 26],
+    ["Rosewind Road", true, "Amnesty", 14]
+  ];
+
+export const malformedCSV = [
+    ["Name", "Age", "isDead"],
+    ["Jordan", 19],
+    ["Anna", 19, false],
+    ["Josh", 20, true, true]
+  ];
