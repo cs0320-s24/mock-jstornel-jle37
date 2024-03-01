1cbda9e11e400ddc93938f621d9888d4f65b68c8
Jenni L <Anna Le>
2024-03-01 17:47:12 -0500

    feat: user can now call view and state without calling the filepath/implemented a filepath state

--- mock/src/components/CommandOutput.tsx
+++ mock/src/components/CommandOutput.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import { view } from "../methods/view";
 import {
   defaultCommandDictionary,
@@ -9,12 +9,19 @@ interface commandOutputProps {
   history: string[];
 }
 
-export function getOutputType(command: string, isVerbose: boolean) {
+export function getOutputType(command: string, isVerbose: boolean, currFile: string, setCurrFile: Dispatch<SetStateAction<string>>) {
   // const [filepath, setFilepath] = useState<string>("");
   const commandArray = command.split(" ");
+  if (commandArray.at(0)! === "load") {
+    setCurrFile(commandArray.at(-1)!);
+  }
+  console.log(commandArray.at(-1)!);
+  // setCurrFile(commandArray.at(-1)!);
+  //currFile = commandArray.at(-1)!;
+  console.log(currFile);
   // setFilepath(commandArray[1]);
 
-  const commandDictionary = defaultCommandDictionary(commandArray.at(-1)!, isVerbose);
+  const commandDictionary = defaultCommandDictionary(currFile, isVerbose);
   // updateDictionary(commandDictionary, "mode", "mode changed");
 
   for (var key in commandDictionary) {
--- mock/src/components/REPL.tsx
+++ mock/src/components/REPL.tsx
@@ -15,14 +15,15 @@ import { REPLInput } from "./REPLInput";
 export default function REPL() {
   // Shared state that holds all the commands submitted.
   const [history, setHistory] = useState<string[]>([]);
-  const [isVerbose, setVerbose] = useState<boolean>(false);
+  const [isVerbose, setVerbose] = useState<boolean>(false); 
+  const [currFile, setCurrFile] = useState<string>("");
 
   return (
     <div className="repl">
       {/*This is where your REPLHistory might go... You also may choose to add it within your REPLInput 
       component or somewhere else depending on your component organization. What are the pros and cons of each? */}
       {/* TODO: Update your REPLHistory and REPLInput to take in new shared state as props */}
-      <REPLHistory history={history} isVerbose={isVerbose} />
+      <REPLHistory history={history} isVerbose={isVerbose} currFile={currFile} setCurrFile={setCurrFile}/>
       <hr></hr>
       <REPLInput history={history} setHistory={setHistory} isVerbose={isVerbose} setVerbose={setVerbose}/>
     </div>
